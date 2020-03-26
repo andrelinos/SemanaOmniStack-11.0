3a78dd6b4f527cbf9abad354485a71138714a2da
Andrelino Silva <andrelinocom@gmail.com>
2020-03-25 23:22:52 -0300

    Projeto web iniciado

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './gobal.css';
+
+import Routes from './routes';
+
+function App() {
+
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
